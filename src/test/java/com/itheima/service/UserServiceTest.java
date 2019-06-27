1537b124fe5e402489ba557d2ee0fbf219468215
sunlei <1724808558@qq.com>
2019-06-27 21:21:57 +0800

    解决冲突

--- src/test/java/com/itheima/service/UserServiceTest.java
+++ src/test/java/com/itheima/service/UserServiceTest.java
@@ -44,6 +44,7 @@ public class UserServiceTest {
         System.out.println("主分支再次制造冲突");
         System.out.println("别人提交");
          System.out.println("别人再次提交");
+         System.out.println("我再次修改");
     }
 
     @Test
